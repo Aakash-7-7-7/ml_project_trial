df9ba55b2545d970163c0feaa7abc96aa9860bc2
Aakash <niranjancharan75@gmail.com>
2025-08-25 05:25:47 +0530

    added setup.py , src

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages,setup
+
+
+
+setup( 
+name='mlproject',
+version='0.0.1',
+author='Aakash',
+author_email='niranjancharan75@gmail.com',
+packages=find_packages(),
+install_requires=['pandas','numpy','seaborn']
+)
+
+
+     
